e18977d86d185c28b87b4dae5c91add6bb53c342
flexicar <writetojay@gmail.com>
2012-10-14 23:00:46 +1000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+import ipydb
+
+requires = ['SQLAlchemy', 'ipython']
+
+setup(
+    name='ipydb',
+    version=ipydb.__version__,
+    description="An IPython extension to help you write and run SQL statements",
+    author='Jay Sweeney',
+    author_email='writetojay@gmail.com',
+    url='http://github.com/jaysw/ipydb',
+    packages=['ipydb'],
+    package_dir={'ipydb': 'ipydb'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires
+)
